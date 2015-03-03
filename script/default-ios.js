464b465219b0144271520d5779e459f4843e9300
unknown <lianwei@lianwei-laptop.fareast.corp.microsoft.com>
2015-03-03 22:13:34 +0800

    Clean

--- script/default-ios.js
+++
@@ -1,9 +0,0 @@
-function init() {
-}
-
-
-$(document).ready(function () {
-    $("article li.section-member a.section-member-name").click(function () {
-        $(this).next("article div.height-container").animate({ 'height': 'toggle' }, 'normal');
-    });
-});
